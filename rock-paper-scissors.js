d0218a7f6c84757913d14179d426c515999da085
stelios <stelios05@gmail.com>
2023-10-15 12:41:59 +0100

    computer choice coded

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+function getComputerChoice(){
+  const choices=[ 'Rock', 'Paper','Scissors'];
+  const index=Math.floor(Math.random()*choices.length);
+  return choices[index];
+}
+
+console.log("computer "+getComputerChoice());
