e22fde011103ce42b8cc0c6c64f85a5617f9abf1
stelios <stelios05@gmail.com>
2023-10-15 12:55:56 +0100

    oneRound works

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,4 +4,38 @@ function getComputerChoice(){
   return choices[index];
 }
 
-console.log("computer "+getComputerChoice());
+
+function playRound( playerSelection, computerSelection){
+  const playerSelectionLc=playerSelection.toLowerCase();
+  const computerSelectionLc=computerSelection.toLowerCase();
+  if(playerSelectionLc === computerSelectionLc){
+    return `Tied! ${playerSelection} ties with ${computerSelection}`;
+  }
+  if(playerSelectionLc === 'rock'){
+    if(computerSelectionLc === 'paper'){
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }else{
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+  }
+  if(playerSelectionLc === 'paper'){
+    if(computerSelectionLc === 'scissors'){
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }else{
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+  }
+  if(playerSelectionLc === 'scissors'){
+    if(computerSelectionLc === 'rock'){
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }else{
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+  }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
